d4043858d35425b9c928acb5b11e85a8b2279171
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-15 17:12:06 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>The Nobody's Blog</h1>
+      <div className="links">
+        <a href="/"> Home</a> | 
+        <a href="/create"> New Blog</a>
+      </div>
+    </nav>
+   );
+}
+export default Navbar;
