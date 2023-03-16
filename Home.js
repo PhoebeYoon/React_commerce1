5ef234ffc28ebff857797e5abbf20e2eb5e88416
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-16 10:45:55 +0900

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 
 const Home = () => {
-  const [a,b] =useState('Hong Gil Dong');
+  // const [a,b] =useState('Hong Gil Dong');
+  const [name, setName ] = useState('Hong Gil Dong')
+  const [age, setAge] =useState(25)
   const handleClick =()=>{
     console.log(a)
-    b('Kim Gil Dong')
+     setName('Kim Gil Dong')
+     setAge(25)
   }
 
   return ( 
     <div className="home">
       <h2>Home </h2>
-      <p>{ a }</p>
+      /* <p>{ a }</p> */
+       <p>{name}</p>
+      <p>{age} years old</p>
       <button onClick={handleClick}>Click here</button> 
     
     </div>
