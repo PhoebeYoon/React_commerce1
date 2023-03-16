6eab468e3fe18d503aa56c19c5181cb5e11d1fab
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-16 14:01:45 +0900

    Add files via upload

---
+++ BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = (props) => {
+  const blogs = props.blogs;
+  console.log(blogs)
+  return ( 
+    <div className="blog-list">
+      { blogs.map((blog)=>(
+          <div className="blog-preview" key={blog.id}> 
+          <h2>{blog.title}</h2>
+          <p>{blog.body}</p>
+          <p>{blog.author}</p>
+          </div>
+        ))}
+    </div>
+   ); }
+export default BlogList;
+
+
