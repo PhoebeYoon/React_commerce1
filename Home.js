412ed36d4241253546dc2028f1faf83fbc652d66
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-17 12:24:04 +0900

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"; 
+import BlogList from "./BlogList";
+
+const Home = () => {
+ const [blogs, setBlog] = useState([
+  {title : 'my article 1', body :'1 lorem ipsum...', author:'Hong', id:1},
+  {title : 'my article 2', body :'2 lorem ipsum...', author:'Kim' , id:2},
+  {title : 'my article 3', body :'3 lorem ipsum...', author:'Lee' , id:3}
+ ])
+
+const handleDelete = (id)=>{
+  const newBlogs = blogs.filter((blog)=> blog.id !==id );
+  setBlog(newBlogs)
+}
+const [name, setName] = useState('Kim')
+
+useEffect(()=>{
+  console.log('useEffect')
+  console.log(name)
+},[name]);
+
+ return ( 
+  <div className="home">
+      <BlogList blogs={blogs} title="All Blogs!" handleRemove={handleDelete}/>
+      <button onClick={()=>setName('Smith')}> Change Name</button>
+      <p>{name}</p>
+  </div>
+ );
+}
+export default Home;
